4630f4e8266621537cb536fdd4315f949ef24ba0
Timur <timur.kokumbaev@gmail.com>
2023-07-04 23:55:20 +0200

    Angular intalliert, components erstellt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product.model';
-import { products as data } from './data/products';
+import { ProductsService } from './services/products.service';
+import { Observable, tap } from 'rxjs';
+import { ModalService } from './services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'hello angular!';
 
-  products: IProduct[] = data
+  // products: IProduct[] = []
+  loading = false
+  // products$: Observable<IProduct[]>
+  term = ''
+
+  constructor(
+    public productsService: ProductsService,
+    public modalService: ModalService
+    ) {
+
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+  //   this.products$ = this.productsService.getAll().pipe(
+  //     tap(() => this.loading = false)
+  //   )
+    this.productsService.getAll().subscribe(products => {
+
+      this.loading = false
+
+    })
+  }
 }
 
 
